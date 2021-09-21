b395bcd0fad8b9f07412027d16d52a7dd2871571
TomasFlorio <tomasflorio00@gmail.com>
2021-09-21 15:32:50 -0300

    clase 5

---
+++ Javascrpt/js/app.js
@@ -0,0 +1,18 @@
+alert('soy una ventana')
+console.log('hola mundo')
+/*variables*/
+var nombre_variable; 
+let otra_variable = 'hola';
+let hayclases = true
+const PI = 3.1416; 
+if(hayclases){/* && = and || = or*/
+    console.log('si hay')
+} 
+
+let elemento = document.getElementById("principal")
+//console.log(elemento)
+elemento.innerHTML="Mi valor";
+elemento.innerHTML="<p>"+otra_variable+"</p>";
+document.write("<h3 class='text-center bg-primary'>SASdasdsa</h3>")
+document.write("<h3>como estan?</h3>")
+parseFloat(" ")
